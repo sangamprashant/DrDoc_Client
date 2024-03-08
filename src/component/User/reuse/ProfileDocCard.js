df09082e2d336eaf9f3d37c27c3818d4946da249
Prashant Srivastav <srivastavp891@gmail.com>
2024-03-09 00:10:10 +0530

    typo

--- src/component/User/reuse/ProfileDocCard.js
+++ src/component/User/reuse/ProfileDocCard.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-const ProfileDocCard = () => {
+const PrescriptionCard = () => {
   return (
     <div className="document-image">
       <div className="card-2">
         <div className="card-content2">
           <h3 className="card-title-profile">Title of the prescription</h3>
-          <p className="card-subtitle-profile">Date 21/12/4533 time:12:34pd  </p>
+          <p className="card-subtitle-profile">
+            Date: 21/12/4533 Time: 12:34 PM
+          </p>
           <div className="card-description-profile">
-            <p>Details of the mediclas presciption </p>
-            <div className="d-flex justify-content-end" ><button className="btn btn-outline-secondary">View in detail</button></div>
+            <p>Details of the medical prescription</p>
+            <div className="d-flex justify-content-end">
+              <button className="btn btn-outline-secondary">
+                View in detail
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -17,4 +23,4 @@ const ProfileDocCard = () => {
   );
 };
 
-export default ProfileDocCard;
+export default PrescriptionCard;
